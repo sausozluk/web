959fd42c1c908adeda9c8a1158929c3b99d238a8
Eray Arslan <relfishere@gmail.com>
2016-09-06 12:07:39 +0300

    api.sausozluk proxied

--- build/tasks/connect.js
+++ build/tasks/connect.js
@@ -1,6 +1,7 @@
-var path = require('path');
 var parse = require('url').parse;
 var fs = require('fs');
+var proxy = require('proxy-middleware');
+var url = require('url');
 
 module.exports = function () {
   this.loadNpmTasks('grunt-contrib-connect');
@@ -10,23 +11,29 @@ module.exports = function () {
         port: 1337,
         base: 'dist',
         middleware: function (connect, options) {
-          return [
-            function (req, res) {
-              var url = parse(req.url);
-              var target = 'dist' + url.pathname;
+          var middlewares = [];
 
-              try {
-                fs.statSync(target);
-                if (target.slice(-1) === "/") {
-                  fs.createReadStream('dist/index.html').pipe(res);
-                } else {
-                  fs.createReadStream(target).pipe(res);
-                }
-              } catch (e) {
+          var proxyOptions = url.parse('https://api.sausozluk.org/');
+          proxyOptions.route = '/api';
+          middlewares.push(proxy(proxyOptions));
+
+          middlewares.push(function (req, res) {
+            var url = parse(req.url);
+            var target = 'dist' + url.pathname;
+
+            try {
+              fs.statSync(target);
+              if (target.slice(-1) === "/") {
                 fs.createReadStream('dist/index.html').pipe(res);
+              } else {
+                fs.createReadStream(target).pipe(res);
               }
+            } catch (e) {
+              fs.createReadStream('dist/index.html').pipe(res);
             }
-          ];
+          });
+
+          return middlewares;
         }
       }
     }
