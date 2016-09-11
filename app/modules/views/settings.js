1a04a21992bbddca16f0d8b386547238509066d6
Eray Arslan <relfishere@gmail.com>
2016-09-11 17:16:23 +0300

    settings page updated

--- app/modules/views/settings.js
+++ app/modules/views/settings.js
@@ -2,6 +2,7 @@ define(function (require, exports, module) {
   var $ = require('jquery');
   var Backbone = require('backbone');
   var SettingsTemplate = require('template!../../templates/settings');
+  var SettingsTabComponent = require('./components/settings-tab');
 
   module.exports = Backbone.View.extend({
     title: 'ayarlar',
@@ -10,8 +11,14 @@ define(function (require, exports, module) {
 
     events: {},
 
+    initialize: function () {
+      this.settingsTabComponent = new SettingsTabComponent();
+      this.settingsTabComponent.render();
+    },
+
     render: function () {
       $(this.el).html(SettingsTemplate());
+      $(this.el).find('.settings').html(this.settingsTabComponent.el);
     }
   });
 });
