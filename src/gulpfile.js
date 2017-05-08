252326a2b1614803dad20e6f1d709e56a1ebeeb0
Okan Vurdu <okanvurdu@gmail.com>
2017-05-08 16:18:50 +0300

    adds dotenv support

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,6 +3,8 @@ var log = require('gulp-util').log;
 var fs = require('fs');
 var dir = './gulp';
 
+require('dotenv').config()
+
 global.name = process.env['SOZLUK_NAME'] || 'saüsözlük';
 global.env = process.env['SOZLUK_ENV'] || 'dev';
 global.apiUrl = process.env['SOZLUK_API_URL'] || 'http://localhost:8080/api/v1';
